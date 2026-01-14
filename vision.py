ca66ef3f0d7e8ccf2a78706a0168558ee629ad74
Pranshu Chauhan <pranshu26092003@gmail.com>
2026-01-14 18:38:23 +0530

    Intial Fucntion Definition

---
+++ vision.py
@@ -0,0 +1,9 @@
+def load_image(path):
+    print(f"Loading image from {path}")
+
+def to_gray(img):
+    print("Converting image to grayscale")
+
+def display(img):
+    print(f"Displaying {img}")
+
